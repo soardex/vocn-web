821e37f4e7df381c699745781f490fff18937e3f
fizzafruit <soardex@vastorigins.net>
2014-05-18 14:21:46 +0800

    -- Move and restructured core javascript.

--- assets/js/voc-user.js
+++ assets/js/voc-user.js
@@ -2,9 +2,10 @@ var gl                            = null;
 var leaf                          = {};
 
 leaf.tag                          = {};
-leaf.tag.ERROR                    = "[240]: "; /// Log Errors
-leaf.tag.WARNING                  = "[220]: "; /// Log Warnings
-leaf.tag.LOG                      = "[210]: "; /// Log Notes
+
+leaf.tag.LOG                      = "[220]: ";
+leaf.tag.ERROR                    = "[240]: ";
+leaf.tag.WARNING                  = "[250]: ";
 
 leaf.shaderProgram                = null;
 leaf.mvMatrix                     = mat4.create();
@@ -13,13 +14,13 @@ leaf.triangleVertexPositionBuffer = null;
 
 leaf.init = function(canvas) {
     try {
-        gl = canvas.getContext("experimental-webgl")
+        gl = canvas.getContext("experimental-webgl") || canvas.getContext("webgl");
         gl.viewportWidth = canvas.width;
         gl.viewportHeight = canvas.height;
     } catch (e) { }
 
     if (!gl) {
-        console.log(this.tag.ERROR + "Unable to initialize WebGL.");
+        this.log(this.tag.ERROR, "Unable to initialize WebGL.");
     }
 }
 
@@ -45,6 +46,7 @@ leaf.getShader = function(gl, id) {
     } else if (shaderSource.type == "x-shader/x-vertex") {
         shader = gl.createShader(gl.VERTEX_SHADER);
     } else {
+        this.log(this.tag.ERROR, "Unknown script type.")
         return null;
     }
 
@@ -52,11 +54,22 @@ leaf.getShader = function(gl, id) {
     gl.compileShader(shader);
 
     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        console.log(this.tag.ERROR + gl.getShaderInfoLog(shader));
+        this.log(this.tag.ERROR, gl.getShaderInfoLog(shader));
         return null;
     }
 
     return shader;
+}
+
+leaf.setMatrixUniforms = function() {
+    gl.uniformMatrix4fv(this.shaderProgram.pMatrix_uniform, false, this.pMatrix);
+    gl.uniformMatrix4fv(this.shaderProgram.mvMatrix_uniform, false, this.mvMatrix);
+}
+
+leaf.log = function(tag, message) {
+    if (tag == this.tag.ERROR) {
+        console.log(tag + message);
+    }
 }
 
 leaf.initShaders = function() {
@@ -66,35 +79,33 @@ leaf.initShaders = function() {
     this.shaderProgram = gl.createProgram();
     gl.attachShader(this.shaderProgram, vertexShader);
     gl.attachShader(this.shaderProgram, fragmentShader);
-    gl.linkProgram(this.shaderProgram);
 
+    gl.linkProgram(this.shaderProgram);
     if (!gl.getProgramParameter(this.shaderProgram, gl.LINK_STATUS)) {
-        console.log(this.tag.ERROR + "Could not initialize shaders.");
-        console.log(this.tag.ERROR + gl.getProgramInfoLog(this.shaderProgram));
+        this.log(this.tag.ERROR, "Could not initialize shaders.");
+        this.log(this.tag.ERROR, gl.getProgramInfoLog(this.shaderProgram));
     }
 
-    gl.useProgram(this.shaderProgram);
     this.shaderProgram.vertexPositionAttribute = gl.getAttribLocation(this.shaderProgram, "aVertexPosition");
-    gl.enableVertexAttribArray(this.shaderProgram.vertexPositionAttribute);
 
     this.shaderProgram.pMatrix_uniform = gl.getUniformLocation(this.shaderProgram, "uPMatrix");
     this.shaderProgram.mvMatrix_uniform = gl.getUniformLocation(this.shaderProgram, "uMVMatrix");
 }
 
-leaf.setMatrixUniforms = function() {
-    gl.uniformMatrix4fv(this.shaderProgram.pMatrix_uniform, false, this.pMatrix);
-    gl.uniformMatrix4fv(this.shaderProgram.mvMatrix_uniform, false, this.mvMatrix);
-}
-
 leaf.initBuffers = function() {
-    this.triangleVertexPositionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, this.triangleVertexPositionBuffer);
     var vertices = [
          0.0,  1.0,  0.0,
         -1.0, -1.0,  0.0,
          1.0, -1.0,  0.0
     ];
+
+    this.triangleVertexPositionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.triangleVertexPositionBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+
+
     this.triangleVertexPositionBuffer.itemSize = 3;
     this.triangleVertexPositionBuffer.numItems = 3;
 }
@@ -103,13 +114,19 @@ leaf.drawScene = function() {
     gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, this.pMatrix);
+    mat4.perspective(45.0, gl.viewportWidth / gl.viewportHeight, 0.1, 750.0, this.pMatrix);
     mat4.identity(this.mvMatrix);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, this.triangleVertexPositionBuffer);
-    gl.vertexAttribPointer(this.shaderProgram.vertexPositionAttribute, this.triangleVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    gl.useProgram(this.shaderProgram);
+
     this.setMatrixUniforms();
-    gl.drawArrays(gl.TRIANGLES, 0, this.triangleVertexPositionBuffer.numItems);
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.triangleVertexPositionBuffer);
+        gl.vertexAttribPointer(this.shaderProgram.vertexPositionAttribute, this.triangleVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(this.shaderProgram.vertexPositionAttribute);
+        gl.drawArrays(gl.TRIANGLES, 0, this.triangleVertexPositionBuffer.numItems);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    gl.useProgram(null);
 }
 
 leaf.main = function() {
@@ -121,6 +138,7 @@ leaf.main = function() {
     if (gl) {
         gl.clearColor(0.0, 0.0, 0.0, 1.0);
         gl.enable(gl.DEPTH_TEST);
+        gl.depthFunc(gl.LEQUAL);
 
         this.drawScene();
     }
