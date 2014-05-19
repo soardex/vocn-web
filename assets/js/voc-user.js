50ff00c41eff24ce56bfeddf72b4d0651b26b75d
fizzafruit <soardex@vastorigins.net>
2014-05-19 11:25:00 +0800

    -- Added some lookAt and animLoop.

--- assets/js/voc-user.js
+++ assets/js/voc-user.js
@@ -8,13 +8,51 @@ leaf.tag.ERROR                    = "[240]: ";
 leaf.tag.WARNING                  = "[250]: ";
 
 leaf.shaderProgram                = null;
+leaf.matrixStack                  = [];
 leaf.mvMatrix                     = mat4.create();
+leaf.mMatrix                      = mat4.create();
+leaf.vMatrix                      = mat4.create();
 leaf.pMatrix                      = mat4.create();
 leaf.triangleVertexPositionBuffer = null;
+leaf.triangleVertexColorBuffer    = null;
+leaf.myTexture                    = null;
+
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame']
+                                || window[vendors[x] + 'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame) {
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+    }
+
+    if (!window.cancelAnimationFrame) {
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+    }
+}());
+
+function animLoop() {
+    requestAnimationFrame(animLoop);
+    leaf.drawScene();
+    leaf.animate();
+}
 
 leaf.init = function(canvas) {
     try {
         gl = canvas.getContext("experimental-webgl") || canvas.getContext("webgl");
+
         gl.viewportWidth = canvas.width;
         gl.viewportHeight = canvas.height;
     } catch (e) { }
@@ -66,6 +104,20 @@ leaf.setMatrixUniforms = function() {
     gl.uniformMatrix4fv(this.shaderProgram.mvMatrix_uniform, false, this.mvMatrix);
 }
 
+leaf.pushMat = function() {
+    var copy = mat4.create();
+    mat4.copy(this.mvMatrix, copy);
+    this.matrixStack.push(copy);
+}
+
+leaf.popMat = function() {
+    if (this.matrixStack.length == 0) {
+        throw "Invalid popMat!";
+    }
+
+    this.mvMatrix = this.matrixStack.pop();
+}
+
 leaf.log = function(tag, message) {
     if (tag == this.tag.ERROR) {
         console.log(tag + message);
@@ -87,6 +139,7 @@ leaf.initShaders = function() {
     }
 
     this.shaderProgram.vertexPositionAttribute = gl.getAttribLocation(this.shaderProgram, "aVertexPosition");
+    this.shaderProgram.vertexColorAttribute = gl.getAttribLocation(this.shaderProgram, "aVertexColor");
 
     this.shaderProgram.pMatrix_uniform = gl.getUniformLocation(this.shaderProgram, "uPMatrix");
     this.shaderProgram.mvMatrix_uniform = gl.getUniformLocation(this.shaderProgram, "uMVMatrix");
@@ -104,29 +157,76 @@ leaf.initBuffers = function() {
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
-
-
     this.triangleVertexPositionBuffer.itemSize = 3;
     this.triangleVertexPositionBuffer.numItems = 3;
+
+    var colors = [
+        1.0, 0.0, 0.0, 1.0,
+        0.0, 1.0, 0.0, 1.0,
+        0.0, 0.0, 1.0, 1.0
+    ];
+
+    this.triangleVertexColorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.triangleVertexColorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    this.triangleVertexColorBuffer.itemSize = 3;
+    this.triangleVertexColorBuffer.numItems = 3;
+}
+
+leaf.initTexture = function() {
+    myTexture = gl.createTexture();
+    myTexture.image = new Image();
+    myTexture.image.onload = function() {
+        (function(texture){
+                gl.bindTexture(gl.TEXTURE_2D, texture);
+                gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+                gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
+                    gl.UNSIGNED_BYTE, texture.image);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+                gl.bindTexture(gl.TEXTURE_2D, null);
+        })(myTexture);
+    }
+
+    myTexture.image.src = "sample.gif";
 }
 
 leaf.drawScene = function() {
     gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    mat4.perspective(45.0, gl.viewportWidth / gl.viewportHeight, 0.1, 750.0, this.pMatrix);
-    mat4.identity(this.mvMatrix);
+    mat4.perspective(45.0, gl.viewportWidth / gl.viewportHeight, 0.1, 1000.0, this.pMatrix);
+    mat4.lookAt(this.vMatrix,
+                [ 0.0,  0.0,  3.0],
+                [ 0.0,  0.0, -5.0],
+                [ 0.0,  1.0,  0.0]);
+    mat4.identity(this.mMatrix);
+    mat4.multiply(this.mvMatrix, this.vMatrix, this.mMatrix);
 
     gl.useProgram(this.shaderProgram);
 
+    this.pushMat();
+    mat4.scale(this.mvMatrix, this.mvMatrix, [1.0, 1.0, 1.0]);
+    mat4.translate(this.mvMatrix, this.mvMatrix, [0.0, 0.0, 0.0]);
     this.setMatrixUniforms();
+
     gl.bindBuffer(gl.ARRAY_BUFFER, this.triangleVertexPositionBuffer);
-        gl.vertexAttribPointer(this.shaderProgram.vertexPositionAttribute, this.triangleVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(this.shaderProgram.vertexPositionAttribute);
-        gl.drawArrays(gl.TRIANGLES, 0, this.triangleVertexPositionBuffer.numItems);
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    gl.vertexAttribPointer(this.shaderProgram.vertexPositionAttribute, this.triangleVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.triangleVertexColorBuffer);
+    gl.vertexAttribPointer(this.shaderProgram.vertexColorAttribute, this.triangleVertexColorBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+    gl.enableVertexAttribArray(this.shaderProgram.vertexPositionAttribute);
+    gl.enableVertexAttribArray(this.shaderProgram.vertexColorAttribute);
+
+    gl.drawArrays(gl.TRIANGLES, 0, this.triangleVertexPositionBuffer.numItems);
+    this.popMat();
 
     gl.useProgram(null);
+}
+
+leaf.animate = function() {
 }
 
 leaf.main = function() {
@@ -136,11 +236,11 @@ leaf.main = function() {
     this.initBuffers();
 
     if (gl) {
-        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clearColor(0.0, 0.7, 0.7, 1.0);
         gl.enable(gl.DEPTH_TEST);
         gl.depthFunc(gl.LEQUAL);
 
-        this.drawScene();
+        animLoop();
     }
 }
 
